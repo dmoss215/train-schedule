e1e3fe964cfdc855daf0bb9862bed6f104a04d17
Daniel Moss <dmoss@stratoswp.com>
2017-12-06 12:14:07 -0500

    finished

--- app.js
+++ app.js
@@ -20,4 +20,66 @@
       let frequency = $("#frequency-input").val().trim();
 
       console.log(trainName)
+
+    // ================  Create newTrain obj to hold variables declared above ============================
+      let newTrain = {
+          name: trainName,
+          dest: destination,
+          firstTime: firstTrainTime,
+          freq: frequency,
+      }
+
+    // =========  Push the new train to the Firebase database
+      database.ref().push(newTrain);
+
+
+    //============ Clear the input boxes ===============================
+    $("#train-name").val("");
+    $("#destination-input").val("");
+    $("#first-train-time").val("");
+    $("#frequency-input").val("");
+
    })
+
+
+
+    // Create Firebase event to add train to the database and a row in html
+
+    database.ref().on("child_added", function (childSnapshot) { 
+        console.log(childSnapshot.val());
+
+        let addedTrain = childSnapshot.val();
+        
+        let newTrainName = addedTrain.name;
+        let newTrainFreq = addedTrain.freq;
+        let newTrainFirstTime = addedTrain.firstTime;
+        let newTrainDest = addedTrain.dest;
+
+        // first time (pushed back one year)
+        let firstTimeConverted = moment(newTrainFirstTime, "hh:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+
+        // Get Current Time
+        let currentTime = moment();
+
+        // difference between the two times
+        var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("Difference in time: " + timeDiff);
+
+        // Time apart (remainder)
+        let tRemainder = timeDiff % newTrainFreq;
+        console.log(tRemainder);
+
+        // Minutes until train
+        let tMinutesTillTrain = newTrainFreq - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // Next Train
+        let nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+        $("#train-table > tbody").append("<tr><td>" + newTrainName + "</td><td>" + newTrainDest + "</td><td>" +  newTrainFreq + "</td> <td>" +  moment(nextTrain).format("hh:mm") + "</td><td>" +  tMinutesTillTrain + "</td></tr>");
+
+
+
+     })
